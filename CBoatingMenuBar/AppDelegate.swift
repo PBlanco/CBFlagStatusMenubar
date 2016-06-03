e84845d364809b1ccb3b32515bcc8c51a339e467
Peter <petercblanco@gmail.com>
2016-06-02 22:22:48 -0400

    fetch flag 6 minute interval

--- CBoatingMenuBar/AppDelegate.swift
+++ CBoatingMenuBar/AppDelegate.swift
@@ -25,37 +25,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let flagStatus = FlagStatus(closed: "C", green: "G", yellow: "Y", red: "R")
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        let icon = NSImage(named: "statusIcon")
-        icon!.template = true;
-        
-        statusItem.image = icon;
-        statusItem.menu = statusMenu;
-        
+        updateFlag();
+        NSTimer.scheduledTimerWithTimeInterval(360, target: self, selector: #selector(AppDelegate.updateFlag), userInfo: nil, repeats: true);
+    }
+    
+    func updateFlag() -> Void {
         let url = NSURL(string: "https://portal2.community-boating.org/pls/apex/CBI_PROD.FLAG_JS")
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
             let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
             let splitString = dataString?.componentsSeparatedByString("\"")
             if (splitString?.count > 1) {
+                var icon = NSImage(named: "yellowFlag")
                 let status = splitString![1];
-                print(status);
-                // TODO: change icon color here. Make into a class that is run every 5 minutes to update flag color 
+                
                 switch status {
-                    case self.flagStatus.closed:
-                        print("closed")
-                    case self.flagStatus.green:
-                        print("green")
-                    case self.flagStatus.yellow:
-                        print("yellow")
-                    case self.flagStatus.red:
-                        print("red")
-                    default:
-                        print("unknown status: " + status)
+                case self.flagStatus.closed:
+                    print("closed")
+                    icon = NSImage(named: "closeFlag")
+                case self.flagStatus.green:
+                    print("green")
+                    icon = NSImage(named: "greenFlag")
+                case self.flagStatus.yellow:
+                    print("yellow")
+                    icon = NSImage(named: "yellowFlag")
+                case self.flagStatus.red:
+                    print("red")
+                    icon = NSImage(named: "redFlag")
+                default:
+                    print("unknown status: " + status)
                 }
+                
+                self.statusItem.image = icon;
+                self.statusItem.menu = self.statusMenu;
             }
         }
         
         task.resume()
-
     }
 
     @IBAction func menuClicked(sender: NSMenuItem) {
